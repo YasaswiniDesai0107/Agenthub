1961132a3a47438cff72f0c22f16a42fdbb85ce6
Yasaswini Desai <yasaswinidesai.k@prodapt.com>
2026-02-16 17:41:12 +0530

    Added Production build for Github Pages deployment again

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
   distDir: 'dist',
+  basePath: '/Agenthub',
   images: {
     unoptimized: true, // Required for static export
   },
